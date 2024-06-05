868b09f1b16e1f4cf5cb37f9f5ac65e4109811e7
P-INI <khionostiara@gmail.com>
2024-06-05 16:47:44 +0900

    Camera_move
    카메라의 이동은 플레이어와 일정 거리를 둔 상태로 마우스 움직임에 따라 공전합니다
    플레이어의 이동 방향은 카메라가 바라보고 있는 방향을 기준으로 결정됩니다

--- OpenSWTermProj/Assets/Scripts/CameraControl.cs
+++ OpenSWTermProj/Assets/Scripts/CameraControl.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public float cameraspeed = 0.0f;
-    public GameObject player;
+    public Transform player;
+    public float distance = 20.0f;
+    public float Xsensitivity = 4.0f;
+    public float Ysensitivity = 2.0f;
+    public float minYAngle = -20f;
+    public float maxYAngle = 80f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+
     void Start()
     {
-        player = GameObject.Find("Sphere");
-        cameraspeed = player.GetComponent<PlayerMove>().speed;
+        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        Vector3 dir = player.transform.position - this.transform.position + new Vector3(0, 15, -20);
-        Vector3 moveVector = new Vector3(dir.x * cameraspeed * Time.deltaTime, dir.y * cameraspeed * Time.deltaTime, dir.z * cameraspeed * Time.deltaTime);
-        this.transform.Translate(moveVector);
+        currentX += Input.GetAxis("Mouse X") * Xsensitivity;
+        currentY -= Input.GetAxis("Mouse Y") * Ysensitivity;
+        currentY = Mathf.Clamp(currentY, minYAngle, maxYAngle);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = player.position + rotation * direction;
+        transform.LookAt(player.position);
     }
 }
